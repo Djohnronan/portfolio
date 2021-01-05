4860facb09b36591ab184e24db9192c83c1e1499
Dalton Ronan <dronan@ualberta.ca>
2021-01-05 15:14:05 -0700

    finish contact page

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,59 +1,35 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Typography, Button } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
+import { Typography, Box } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+import './contact.css'
+
 export default function Contact() {
-    return (
-        <>
-        <Grid container style={{ paddingTop: 200, paddingLeft: 100, paddingRight: 100}}>
-            <Grid item md={6} xs={12} style={{ display: 'flex' }} >
-                <Typography style={{ fontWeight: 600 }} variant='h5'>Any questions?</Typography>&nbsp;
-                <Typography style={{ fontWeight: 600 }} variant='h5' color='primary'>Contact me.</Typography>
-            </Grid>
-            <Grid item md={6} xs={12} >
-                {/* <Typography style={{ fontWeight: 600 }} variant='h5'>Contact Me</Typography> */}
-                <TextField 
-                    id="form-name" 
-                    label="Name" 
-                    variant="outlined"
-                    size="small" 
-                    autoFocus 
-                    fullWidth
-                    margin="normal"
-                    required />
-                <TextField 
-                    id="form-email" 
-                    label="Email" 
-                    variant="outlined"
-                    size="small"
-                    fullWidth
-                    margin="normal"
-                    required />
-                <TextField 
-                    id="form-subject" 
-                    label="Subject (optional)" 
-                    variant="outlined" 
-                    fullWidth
-                    margin="normal"
-                    size="small" />
-                <TextField 
-                    id="form-message" 
-                    label="Message" 
-                    variant="outlined" 
-                    required 
-                    multiline 
-                    fullWidth
-                    margin="normal"
-                    size="small"
-                    rows={10} />
-                <Button variant="contained" color="primary">Send</Button>
-            </Grid>
-            <FontAwesomeIcon style={{ padding: 4, marginTop: -35 }} icon={faGithub} size="2x"/>
-            <FontAwesomeIcon style={{ padding: 4, marginTop: -35 }} icon={faLinkedin} size="2x"/>
+
+  return (
+    <>
+      <Grid container style={{ paddingTop: 200, paddingLeft: 100, paddingRight: 100 }} className="message-container">
+        <Grid item md={6} sm={12} xs={12} className="message">
+          <Typography style={{ fontWeight: 600 }} variant='h4'>Any questions?</Typography>&nbsp;&nbsp;&nbsp;
+          <Typography style={{ fontWeight: 600 }} variant='h4' color='primary' id="contact-me" >Contact me.</Typography>
         </Grid>
-        </>
-    )
+        <Grid item md={6} sm={12} xs={12}>
+          <Box>
+            <Typography style={{ fontSize: 20 }}>dronan@ualberta.ca</Typography>
+            <Typography style={{ fontSize: 20 }}>403.363.7393</Typography>
+          </Box>
+          <Box style={{ paddingTop: 24 }}>
+            <a style={{ color: 'inherit' }} rel="noopener noreferrer" target="_blank" href="https://github.com/Djohnronan/">
+              <FontAwesomeIcon style={{ padding: 4, paddingRight: 20 }} icon={faGithub} size="3x" onClick={() => console.log('github')} />
+            </a>
+            <a style={{ color: 'inherit' }} rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/dalton-ronan-bb6084135/">
+              <FontAwesomeIcon style={{ padding: 4 }} icon={faLinkedin} size="3x" />
+            </a>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
